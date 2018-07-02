fa95fa541faf899b7c357ff7bfe0a700aa981461
Jacob Pashman <jacobpashman@gmail.com>
2018-07-02 15:31:26 -0700

    Initial Commit

---
+++ CryptoTrack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoTrack
+//
+//  Created by Jacob Pashman on 7/2/18.
+//  Copyright © 2018 jacobpashman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
